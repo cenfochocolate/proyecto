30a8d513dc7dd22faf158a86de955740aa93ea79
brandon <Rutch>
2019-04-16 10:42:46 -0600

    Cambios en el perfil de usuario

--- public/js/controlador_perfil_padres.js
+++ public/js/controlador_perfil_padres.js
@@ -13,6 +13,8 @@ let papellido = document.querySelector('#primer_apellido');
 let sapellido = document.querySelector('#segundo_apellido');
 let chijos = document.querySelector('#cantidad_de_hijos');
 let ntel = document.querySelector('#num_telefono');
+let mail = document.querySelector('#email');
+let direccion = document.querySelector('#direccion');
 
 foto_perfil.src = padre['url_foto'];
 nombre.innerHTML=padre['nombre'];
@@ -20,3 +22,5 @@ papellido.innerHTML= padre['primer_apellido'];
 sapellido.innerHTML=padre['segundo_apellido'];
 chijos.innerHTML=padre['cantidad_de_hijos'];
 ntel.innerHTML=padre['telefono'];
+mail.innerHTML=padre['correo'];
+direccion.innerHTML=padre['direccion'];
