157ccacec9243f144bca404a04a76ca269ee4ffa
eilin venegas <evenegass@ucenfotec.ac.cr>
2019-04-23 11:04:31 -0500

    editar, eliminar

--- public/js/controlador_listar_registrar.js
+++ public/js/controlador_listar_registrar.js
@@ -24,7 +24,7 @@ function mostrar_datos(){
    let celda_configuracion = fila.insertCell();
 
    let boton_editar = document.createElement('a');
-   boton_editar.textContent ='Editar';
+   boton_editar.innerHTML ='<i class="fas fa-edit"></i>';
    boton_editar.href=`actualizar_cita.html?id_cita=${cita[i]['_id']}`
 
    celda_configuracion.appendChild(boton_editar);
@@ -33,7 +33,7 @@ function mostrar_datos(){
 
    let boton_eliminar = document.createElement('a');
    boton_eliminar.href= '#';
-   boton_eliminar.innerHTML= 'Eliminar';
+   boton_eliminar.innerHTML= '<i class="fas fa-trash-alt"></i>';
    boton_eliminar.dataset.id= cita[i]['_id'];
    boton_eliminar.addEventListener('click', confirmar_borrado);
    celda_eliminar.appendChild(boton_eliminar);
