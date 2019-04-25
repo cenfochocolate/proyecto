6668d2c7557c85b1b7c32e8defd6fd27e0728e93
eilin venegas <evenegass@ucenfotec.ac.cr>
2019-04-24 21:37:50 -0500

    cambios

--- public/js/servicio_perfil_institucion.js
+++ public/js/servicio_perfil_institucion.js
@@ -52,12 +52,32 @@ let listar_contacto = () => {
     });
     return lista_contacto;
 };
-let registrar_contacto = (pid, pnombre, pidentificacion, pdepartamento, ptelefono, pcorreo, pextension, pimagen) => {
+function borrar_contacto(pid){
+  $.ajax({
+      url: 'http://localhost:4000/api/borrar_contacto',
+      method: 'POST',
+      contentType: "application/x-www-form-urlencoded; charset=utf-8",
+      data: {
+          id: pid
+      },
+      beforeSend: function beforeSend(){
+
+      },
+      success: function success(response){
+
+      },
+      error: function error(_error){
+          console.log("Request fail error: " + _error);
+
+      }
+  });
+};
+let registrar_contacto = (pid_institucion, pnombre, pidentificacion, pdepartamento, ptelefono, pcorreo, pextension, pimagen) => {
     let request = $.ajax({
         url: "http://localhost:4000/api/registrar_contacto",
         method: "POST",
         data: {
-
+          id_institucion:pid_institucion,
           nombre: pnombre,
           identificacion: pidentificacion,
           departamento : pdepartamento,
@@ -91,6 +111,80 @@ let registrar_contacto = (pid, pnombre, pidentificacion, pdepartamento, ptelefon
 
 
 };
+let deshabilitar_contacto = (pid) => {
+  let request = $.ajax({
+      url: "http://localhost:4000/api/actualizar_contacto",
+      method: "POST",
+      data: {
+        estado : "Desactivo",
+         id: pid
+           },
+           
+      contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+      dataType: "json",
+      async: false
+  });
+
+  request.done(function (res) {
+    
+      swal.fire({
+          type: 'success',
+          title: 'Contacto deshabiitado correctamente',
+          text: res.msg,
+          onClose: () => {
+            window.location.href = 'perfil_institucion.html';
+          }    
+      });
+
+  });
+
+  request.fail(function (res) {
+      swal.fire({
+          type: 'error',
+          title: 'Contacto no deshabilitado',
+          text: res.msg
+         
+      });
+  });
+};
+
+let habilitar_contacto = (pid) => {
+  let request = $.ajax({
+      url: "http://localhost:4000/api/actualizar_contacto",
+      method: "POST",
+      data: {
+        estado : "Activo",
+         id: pid
+           },
+           
+      contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+      dataType: "json",
+      async: false
+  });
+
+  request.done(function (res) {
+    
+      swal.fire({
+          type: 'success',
+          title: 'Contacto activado correctamente',
+          text: res.msg,
+          onClose: () => {
+            window.location.href = 'perfil_institucion.html';
+          }    
+      });
+
+  });
+
+  request.fail(function (res) { 
+      swal.fire({
+          type: 'error',
+          title: 'contacto activada',
+          text: res.msg
+         
+      });
+  });
+};
+
 
 
 let registrar_rs = (pfacebook, pinstagram, ptwitter, pemail, pyoutube) =>{
@@ -141,6 +235,7 @@ let listar_rs = () => {
   });
   return lista_rs;
 };
+
 
 let registrar_comentario = (pid_usuario, pid_institucion, pcomentario)=>{
   let request = $.ajax({
@@ -298,7 +393,7 @@ function buscar_institucion(pid) {
 
     request.done(function (msg) {
         lugar = msg.institucion;
-    })
+    }) 
 
     return lugar;
 };
