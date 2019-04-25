d850baac5242e91c2b99744393afbc9d78329c8d
george <gcerdasc@ucenfotec.ac.cr>
2019-04-25 03:03:10 -0600

    iconos

--- public/js/controlador_listarsadicional.js
+++ public/js/controlador_listarsadicional.js
@@ -35,7 +35,7 @@ function mostrar_datos(){
         let celda_configuracion = fila.insertCell();
       
         let boton_editar = document.createElement('a');
-        boton_editar.textContent = 'Editar';
+        boton_editar.innerHTML ='<i class="fas fa-edit"></i>';
         boton_editar.href = `actualizar_servicio.html?id_servicio=${sadicional[i]['_id']}`;
 
         celda_configuracion.appendChild(boton_editar);
@@ -44,7 +44,7 @@ function mostrar_datos(){
 
         let boton_eliminar = document.createElement('a');
         boton_eliminar.href= '#';
-        boton_eliminar.innerHTML= 'Eliminar';
+        boton_eliminar.innerHTML= '<i class="fas fa-trash-alt"></i>';
         boton_eliminar.dataset.id= sadicional[i]['_id'];
         boton_eliminar.addEventListener('click', confirmar_borrado);
         celda_eliminar.appendChild(boton_eliminar);
