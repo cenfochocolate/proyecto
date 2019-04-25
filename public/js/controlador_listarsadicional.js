0155747dba2167dc469a7595aab3715c8ee50dad
Julissa <jgaraym@gmail.com>
2019-04-25 10:24:14 -0500

    ....

--- public/js/controlador_listarsadicional.js
+++ public/js/controlador_listarsadicional.js
@@ -17,13 +17,14 @@ function mostrar_datos() {
   let filtro = inputFiltro.value;
   tabla.innerHTML = '';
 
+
+  let fila = tabla.insertRow();
   for (let i = 0; i < sadicional.length; i++) {
 
     if (sadicional[i]['nombre'].toLowerCase().includes(filtro.toLowerCase())) {
 
       if (id_institucion == sadicional[i]['id_institucion']) {
 
-        let fila = tabla.insertRow();
 
         fila.insertCell().innerHTML = sadicional[i]['nombre'];
         fila.insertCell().innerHTML = sadicional[i]['descripcion'];
@@ -42,7 +43,7 @@ function mostrar_datos() {
         let celda_configuracion = fila.insertCell();
 
         let boton_editar = document.createElement('a');
-        boton_editar.innerHTML ='<i class="fas fa-edit"></i>';
+        boton_editar.innerHTML = '<i class="fas fa-edit"></i>';
         boton_editar.href = `actualizar_servicio.html?id_servicio=${sadicional[i]['_id']}`;
 
         celda_configuracion.appendChild(boton_editar);
@@ -50,50 +51,13 @@ function mostrar_datos() {
         let celda_eliminar = fila.insertCell();
 
         let boton_eliminar = document.createElement('a');
-        boton_eliminar.href= '#';
-        boton_eliminar.innerHTML= '<i class="fas fa-trash-alt"></i>';
-        boton_eliminar.dataset.id= sadicional[i]['_id'];
+        boton_eliminar.href = '#';
+        boton_eliminar.innerHTML = '<i class="fas fa-trash-alt"></i>';
+        boton_eliminar.dataset.id = sadicional[i]['_id'];
         boton_eliminar.addEventListener('click', confirmar_borrado);
         celda_eliminar.appendChild(boton_eliminar);
 
         //deshabilitar 
-        let celda_deshabilitar = fila.insertCell();
-        let boton_deshabilitar = document.createElement('a');
-        boton_deshabilitar.innerHTML= '<i class="fas fa-eye-slash"></i>';
-        boton_deshabilitar.dataset.id = sadicional[i]['_id'];
-        boton_deshabilitar.addEventListener('click', confirmar_deshabilitar);
-        celda_deshabilitar.appendChild(boton_deshabilitar);
-
-        //habilitar
-        let celda_habilitar = fila.insertCell();
-      let boton_habilitar = document.createElement('a');
-      boton_habilitar.innerHTML= '<i class="fas fa-eye"></i>';
-      boton_habilitar.dataset.id =sadicional[i]['_id'];
-      boton_habilitar.addEventListener('click', confirmar_habilitar);
-      celda_habilitar.appendChild(boton_habilitar);
-      }
-  }else{
-    if (sadicional[i]['nombre'].toLowerCase().includes(filtro.toLowerCase())&&sadicional[i]['id_institucion'] == institucion && sadicional[i]['estado'] == "Activo")
-
-         
-
-        fila.insertCell().innerHTML = sadicional[i]['nombre'];
-        fila.insertCell().innerHTML = sadicional[i]['descripcion'];
-        let imagen = document.createElement('img');
-        imagen.classList.add('imagenTabla');
-        if(sadicional[i]['imagen']){
-          imagen.src = sadicional[i]['imagen'];
-        }else{
-          imagen.src = './imgs/imgph.jpg';
-        }
-        fila.insertCell().appendChild(imagen);
-
-        fila.insertCell().innerHTML = sadicional[i]['estado'];
-
-    }
-  };
-};
-
         let celda_deshabilitar = fila.insertCell();
         let boton_deshabilitar = document.createElement('a');
         boton_deshabilitar.innerHTML = '<i class="fas fa-eye-slash"></i>';
@@ -101,58 +65,120 @@ function mostrar_datos() {
         boton_deshabilitar.addEventListener('click', confirmar_deshabilitar);
         celda_deshabilitar.appendChild(boton_deshabilitar);
 
+        //habilitar
         let celda_habilitar = fila.insertCell();
         let boton_habilitar = document.createElement('a');
         boton_habilitar.innerHTML = '<i class="fas fa-eye"></i>';
         boton_habilitar.dataset.id = sadicional[i]['_id'];
         boton_habilitar.addEventListener('click', confirmar_habilitar);
         celda_habilitar.appendChild(boton_habilitar);
+      }
+    } else {
+      if (sadicional[i]['nombre'].toLowerCase().includes(filtro.toLowerCase()) && sadicional[i]['id_institucion'] == institucion && sadicional[i]['estado'] == "Activo")
 
+
+
+        fila.insertCell().innerHTML = sadicional[i]['nombre'];
+      fila.insertCell().innerHTML = sadicional[i]['descripcion'];
+      let imagen = document.createElement('img');
+      imagen.classList.add('imagenTabla');
+      if (sadicional[i]['imagen']) {
+        imagen.src = sadicional[i]['imagen'];
       } else {
+        imagen.src = './imgs/imgph.jpg';
+      }
+      fila.insertCell().appendChild(imagen);
 
-function confirmar_deshabilitar(){
-  let id= this.dataset.id;
-  Swal.fire({
-    title:'¿Está seguro que desea deshabilitar?',
-    type: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Sí, estoy seguro.'
-}).then((result)=>{
-  if(result.value){
-    deshabilitar_servicio(id);
-    sadicional = listar_sadicional();
-    mostrar_datos();
-    Swal.fire({
-      title:'¡Deshabilitado con éxito!',
-      type:'success'
-    })
-  }
-})
+      fila.insertCell().innerHTML = sadicional[i]['estado'];
+
+      let celda_deshabilitar = fila.insertCell();
+      let boton_deshabilitar = document.createElement('a');
+      boton_deshabilitar.innerHTML = '<i class="fas fa-eye-slash"></i>';
+      boton_deshabilitar.dataset.id = sadicional[i]['_id'];
+      boton_deshabilitar.addEventListener('click', confirmar_deshabilitar);
+      celda_deshabilitar.appendChild(boton_deshabilitar);
+
+      let celda_habilitar = fila.insertCell();
+      let boton_habilitar = document.createElement('a');
+      boton_habilitar.innerHTML = '<i class="fas fa-eye"></i>';
+      boton_habilitar.dataset.id = sadicional[i]['_id'];
+      boton_habilitar.addEventListener('click', confirmar_habilitar);
+      celda_habilitar.appendChild(boton_habilitar);
+
+
+    }
+  };
 };
 
 
-function confirmar_habilitar(){
-  let id= this.dataset.id;
+
+
+function confirmar_borrado() {
+  let id = this.dataset.id;
   Swal.fire({
-    title:'¿Está seguro que desea habilitar?',
+    title: '¿Está seguro que desea eliminar?',
     type: 'warning',
     showCancelButton: true,
     confirmButtonColor: '#3085d6',
     cancelButtonColor: '#d33',
     confirmButtonText: 'Sí, estoy seguro.'
-}).then((result)=>{
-  if(result.value){
-    habilitar_servicio(id);
-   sadicional = listar_sadicional();
-    mostrar_datos();
-    Swal.fire({
-      title:'¡Habilitado con éxito!',
-      type:'success'
-    })
-  }
-})
+  }).then((result) => {
+    if (result.value) {
+      borrar_servicio(id);
+      sadicional = listar_sadicional();
+      mostrar_datos();
+      Swal.fire({
+        title: '¡Matrícula eliminada!',
+        text: 'La matrícula fue eliminada con éxito.',
+        type: 'success'
+      })
+    }
+  })
+};
+
+function confirmar_deshabilitar() {
+  let id = this.dataset.id;
+  Swal.fire({
+    title: '¿Está seguro que desea deshabilitar?',
+    type: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Sí, estoy seguro.'
+  }).then((result) => {
+    if (result.value) {
+      deshabilitar_servicio(id);
+      sadicional = listar_sadicional();
+      mostrar_datos();
+      Swal.fire({
+        title: '¡Deshabilitado con éxito!',
+        type: 'success'
+      })
+    }
+  })
+};
+
+
+function confirmar_habilitar() {
+  let id = this.dataset.id;
+  Swal.fire({
+    title: '¿Está seguro que desea habilitar?',
+    type: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Sí, estoy seguro.'
+  }).then((result) => {
+    if (result.value) {
+      habilitar_servicio(id);
+      sadicional = listar_sadicional();
+      mostrar_datos();
+      Swal.fire({
+        title: '¡Habilitado con éxito!',
+        type: 'success'
+      })
+    }
+  })
 };
 
 mostrar_datos();
